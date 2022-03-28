863f966fa5d621d37930815f8bc58d7ab398ded7
kukabi <kutsalbilgin@gmail.com>
2022-03-28 21:28:04 +0300

    Maintenance mode.

--- src/client/chainviz.ts
+++ src/client/chainviz.ts
@@ -154,10 +154,12 @@ class ChainViz {
     }
 
     async init() {
-        this.setLoadingStatus(":: connecting to services ::");
+        this.setLoadingStatus(
+            "::&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;UNDER MAINTENANCE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;::<br>:: => 30.03.2022 10:00 UTC <= ::"
+        );
         this.displayVersion();
-        this.getInitialBlocks();
-        this.validatorListClient.connect();
+        //this.getInitialBlocks();
+        //this.validatorListClient.connect();
     }
 
     private async startScene() {
