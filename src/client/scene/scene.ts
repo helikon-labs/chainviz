1fabcd5e453fbf4658dbc6350f7f8491218f5d4c
kukabi <kutsalbilgin@gmail.com>
2022-07-28 09:25:34 +0300

    Fix a resolution issue that caused the render to display blurry on some devices.

--- src/client/scene/scene.ts
+++ src/client/scene/scene.ts
@@ -72,6 +72,7 @@ class ChainVizScene {
         // renderer
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         document.body.appendChild(this.renderer.domElement);
